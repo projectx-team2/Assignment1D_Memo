0590476b5ecc31ea060a11b54eff18b814dbeb46
junior-bash <bobkalala12@gmail.com>
2022-09-21 14:37:52 +0200

    Noks helped me

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+  }
+  
+  function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(1);
+  }
